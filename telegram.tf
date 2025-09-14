1c5b720caebc991de5c60f63fc490462f7b350c8
Denis Ustinov <proff.ust@gmail.com>
2025-09-14 22:42:18 +0200

    add telegram commands

--- telegram.tf
+++ telegram.tf
@@ -4,3 +4,32 @@ resource "telegram_bot_webhook" "webhook" {
     google_cloudfunctions2_function.telegram_bot_function,
   ]
 }
+
+resource "telegram_bot_commands" "ai-bot-commands" {
+  commands = [
+    {
+      command = "start",
+      description = "Поздороваться"
+    },
+    {
+      command = "image",
+      description = "Генерация изображения по текстовому промпту"
+    },
+    {
+      command = "new_session",
+      description = "Начать новый сеанс общения с ботом"
+    },
+    {
+      command = "get_model",
+      description = "Вывод текущей используемой модели"
+    },
+    {
+      command = "set_model",
+      description = "Установка модели для общения с ботом"
+    },
+    {
+      command = "help",
+      description = "Поздороваться и рассказать о себе"
+    }
+  ]
+}
